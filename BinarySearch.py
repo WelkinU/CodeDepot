436bae206b3361505c2bcb2725c424d3932751df
WelkinU <welkinuttaro@gmail.com>
2022-05-08 18:36:31 -0400

    Converting tabs to spaces

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,62 +1,62 @@
 ''' Performs binary search to find value of x such that:
-	target_output = func(x)
-	or alternatively: x = func^-1(target_output)
+    target_output = func(x)
+    or alternatively: x = func^-1(target_output)
 
-	Was re-writing this code too much, so I made a general module for re-use
+    Was re-writing this code too much, so I made a general module for re-use
 '''
 
 import math #just needed for examples section
 
 def binary_search(func, target_output, lower, upper, tolerance = 0.0001, max_iter = 50):
-	''' Performs binary search to find value of x such that:
-	target_output = func(x)
-	or alternatively: x = func^-1(target_output)
+    ''' Performs binary search to find value of x such that:
+    target_output = func(x)
+    or alternatively: x = func^-1(target_output)
 
-	Arguments:
-		func {function} -- This is the function to use
-		target_output {int} -- The target value of the function
-		lower {float} -- Initial lower bound for search
-		upper {float} -- Initial upper bound for search
+    Arguments:
+        func {function} -- This is the function to use
+        target_output {int} -- The target value of the function
+        lower {float} -- Initial lower bound for search
+        upper {float} -- Initial upper bound for search
 
-	Optional Arguments:
-		tolerance {float} -- Once abs(func(x) - target_output) < tolerance, then the search stops
-		max_iter {int} -- Maximum number of binary search iterations (to prevent infinite loop if no convergance)
-	'''
+    Optional Arguments:
+        tolerance {float} -- Once abs(func(x) - target_output) < tolerance, then the search stops
+        max_iter {int} -- Maximum number of binary search iterations (to prevent infinite loop if no convergance)
+    '''
 
-	if abs(lower) > abs(upper):
-		upper, lower = lower, upper #swap upper and lower
+    if abs(lower) > abs(upper):
+        upper, lower = lower, upper #swap upper and lower
 
-	guess = (lower + upper) / 2
-	for i in range(max_iter):
-		estimate = func(guess)
-		#print(f'Iter {i} Guess {guess} F(guess) = {estimate}, L = {lower}, U = {upper}')
-		if abs(target_output - estimate) < tolerance:
-			return guess
+    guess = (lower + upper) / 2
+    for i in range(max_iter):
+        estimate = func(guess)
+        #print(f'Iter {i} Guess {guess} F(guess) = {estimate}, L = {lower}, U = {upper}')
+        if abs(target_output - estimate) < tolerance:
+            return guess
 
-		if estimate < target_output:
-			lower = guess
-			guess = (lower + upper)/2	
-		else:
-			upper = guess
-			guess = (lower + upper)/2
+        if estimate < target_output:
+            lower = guess
+            guess = (lower + upper)/2   
+        else:
+            upper = guess
+            guess = (lower + upper)/2
 
-	return guess
+    return guess
 
 if __name__ == '__main__':
 
-	########################################
-	#------------ EXAMPLES ZONE ------------
-	########################################
+    ########################################
+    #------------ EXAMPLES ZONE ------------
+    ########################################
 
-	#Solves "x^2 = 25" converges to 5
-	
-	est = binary_search(func = lambda x: x**2, target_output = 25, lower = 0, upper = 25)
-	print(f'Estimate: {est}')
-	print('\n-----------------------------------------------------------------------------\n')
+    #Solves "x^2 = 25" converges to 5
+    
+    est = binary_search(func = lambda x: x**2, target_output = 25, lower = 0, upper = 25)
+    print(f'Estimate: {est}')
+    print('\n-----------------------------------------------------------------------------\n')
 
-	#Solves "2^(x/2) + x = 0" converges -0.7666015625
-	f = lambda x: 2 ** (x/2) + x
-	est = binary_search(func = f, target_output = 0, lower = - 10, upper = 10)
+    #Solves "2^(x/2) + x = 0" converges -0.7666015625
+    f = lambda x: 2 ** (x/2) + x
+    est = binary_search(func = f, target_output = 0, lower = - 10, upper = 10)
 
-	print(f'Estimate: {est}')
+    print(f'Estimate: {est}')
 
--- GeneralizedThreesChallengeSolver.py
+++ GeneralizedThreesChallengeSolver.py
@@ -16,163 +16,163 @@ import operator #for basic Python functions. Docs: https://docs.python.org/3/lib
 
 
 def find_combinations(starting_tuple = (3,3,3), output_lower_bound = 0, output_upper_bound = 10, max_iter = 1000):
-	''' Uses a breadth first search to search the space of possible tuple mutations.
-	Outputs the integer solutions and the steps to achieve that solution.
+    ''' Uses a breadth first search to search the space of possible tuple mutations.
+    Outputs the integer solutions and the steps to achieve that solution.
 
-	Arguments:
-	starting_tuple {tuple} -- Tuple representing the initial problem state. Tuple can be any length > 0 (not just length 3).
-	output_lower_bound {int} -- The lower limit of integer solutions to print() out.
-	output_upper_bound {int} -- The up limit of integer solutions to print() out. Used to avoid excessive solution printing
-									of integer output like 720 = 6!, that people may not care about
-	max_iter {int} -- The maximum number of BFS iterations (to cap runtime in case of very large search spaces)
+    Arguments:
+    starting_tuple {tuple} -- Tuple representing the initial problem state. Tuple can be any length > 0 (not just length 3).
+    output_lower_bound {int} -- The lower limit of integer solutions to print() out.
+    output_upper_bound {int} -- The up limit of integer solutions to print() out. Used to avoid excessive solution printing
+                                    of integer output like 720 = 6!, that people may not care about
+    max_iter {int} -- The maximum number of BFS iterations (to cap runtime in case of very large search spaces)
 
-	'''
-	assert output_lower_bound <= output_upper_bound
+    '''
+    assert output_lower_bound <= output_upper_bound
 
-	#########################################################
-	#------------------ ADD FUNCTIONS HERE ------------------
-	#########################################################
-	#These functions have 2 inputs and 1 output
-	join_functions = [
-		operator.add,
-		operator.sub,
-		operator.mul,
-		special_divide, #handles divide by zero, maintains int typing
-	]
+    #########################################################
+    #------------------ ADD FUNCTIONS HERE ------------------
+    #########################################################
+    #These functions have 2 inputs and 1 output
+    join_functions = [
+        operator.add,
+        operator.sub,
+        operator.mul,
+        special_divide, #handles divide by zero, maintains int typing
+    ]
 
-	#These functions have 1 input and 1 output
-	inplace_modifier_functions = [
-		math.factorial,
-		math.sqrt,
-	]
-	#########################################################
+    #These functions have 1 input and 1 output
+    inplace_modifier_functions = [
+        math.factorial,
+        math.sqrt,
+    ]
+    #########################################################
 
-	q = queue.SimpleQueue()
-	q.put(starting_tuple)
+    q = queue.SimpleQueue()
+    q.put(starting_tuple)
 
-	tuple_library = {starting_tuple: None}
+    tuple_library = {starting_tuple: None}
 
-	i = 0
-	while not q.empty():
-		i += 1
-		if i >= max_iter:
-			print('Exceeded iteration limit. Breaking search.')
-			break
+    i = 0
+    while not q.empty():
+        i += 1
+        if i >= max_iter:
+            print('Exceeded iteration limit. Breaking search.')
+            break
 
-		tuple_to_mutate = q.get()
-		new_tuple_list = []
-		if len(tuple_to_mutate) > 1: 
-			#create all mutations of the tuple_to_mutate that can be created with functions in list join_functions
-			for func in join_functions:				
-				for idx in range(len(tuple_to_mutate) - 1):
-					
-					try:
-						#generate next tuple via mutating with a joining function
-						new_tuple = (*tuple_to_mutate[0:idx],
-									func(tuple_to_mutate[idx], tuple_to_mutate[idx + 1]),
-									*tuple_to_mutate[idx+2:]
-									)
+        tuple_to_mutate = q.get()
+        new_tuple_list = []
+        if len(tuple_to_mutate) > 1: 
+            #create all mutations of the tuple_to_mutate that can be created with functions in list join_functions
+            for func in join_functions:             
+                for idx in range(len(tuple_to_mutate) - 1):
+                    
+                    try:
+                        #generate next tuple via mutating with a joining function
+                        new_tuple = (*tuple_to_mutate[0:idx],
+                                    func(tuple_to_mutate[idx], tuple_to_mutate[idx + 1]),
+                                    *tuple_to_mutate[idx+2:]
+                                    )
 
-						new_tuple_list.append(new_tuple)
-					except ZeroDivisionError:
-						pass  #catch zero divides, and zero mod() divides
-					except Exception as e:
-						print(f'You have an error in your join_function: {e}')
+                        new_tuple_list.append(new_tuple)
+                    except ZeroDivisionError:
+                        pass  #catch zero divides, and zero mod() divides
+                    except Exception as e:
+                        print(f'You have an error in your join_function: {e}')
 
-		#create all mutations of the tuple to mutate, this time using only inplace modifier functions
-		for func in inplace_modifier_functions:
-			for idx in range(len(tuple_to_mutate)):
+        #create all mutations of the tuple to mutate, this time using only inplace modifier functions
+        for func in inplace_modifier_functions:
+            for idx in range(len(tuple_to_mutate)):
 
-				val = func(tuple_to_mutate[idx])
-				if float(val).is_integer(): #add this check so sqrt(4) -> 2 (int) instead of 2.0 (float)
-					val = int(val)
+                val = func(tuple_to_mutate[idx])
+                if float(val).is_integer(): #add this check so sqrt(4) -> 2 (int) instead of 2.0 (float)
+                    val = int(val)
 
-				new_tuple = (*tuple_to_mutate[0:idx], 
-							val,
-							*tuple_to_mutate[idx+1:])
-				
-				new_tuple_list.append(new_tuple)
+                new_tuple = (*tuple_to_mutate[0:idx], 
+                            val,
+                            *tuple_to_mutate[idx+1:])
+                
+                new_tuple_list.append(new_tuple)
 
-		#check if mutations are valid and undiscovered, if they are, add them to tuple library and queue for future mutation
-		for new_tuple in new_tuple_list:
-			if new_tuple not in tuple_library.keys():
+        #check if mutations are valid and undiscovered, if they are, add them to tuple library and queue for future mutation
+        for new_tuple in new_tuple_list:
+            if new_tuple not in tuple_library.keys():
 
-				# check if we converged to a new integer solution, then print out our discovery!
-				if len(new_tuple) == 1 and new_tuple[0] is not None and float(new_tuple[0]).is_integer(): 
-					if output_lower_bound<= new_tuple[0] <= output_upper_bound:
-						print(f'Discovered {new_tuple[0]}')
-						temp = tuple_to_mutate
-						while temp is not None:
-							print(f'<- {temp} ', end = '')
-							temp = tuple_library[temp]
-						print('')
+                # check if we converged to a new integer solution, then print out our discovery!
+                if len(new_tuple) == 1 and new_tuple[0] is not None and float(new_tuple[0]).is_integer(): 
+                    if output_lower_bound<= new_tuple[0] <= output_upper_bound:
+                        print(f'Discovered {new_tuple[0]}')
+                        temp = tuple_to_mutate
+                        while temp is not None:
+                            print(f'<- {temp} ', end = '')
+                            temp = tuple_library[temp]
+                        print('')
 
-						q.put(new_tuple) #we do want to add the tuple back to the queue because inplace operations can still be used
+                        q.put(new_tuple) #we do want to add the tuple back to the queue because inplace operations can still be used
 
-					tuple_library[new_tuple] = tuple_to_mutate #add to library of tuples discovered
-					continue
+                    tuple_library[new_tuple] = tuple_to_mutate #add to library of tuples discovered
+                    continue
 
-				#otherwise, check if this tuple is valid, if so add it to the tuple library, and
-				#add to the queue of new tuples for future mutation
-				if validate_tuple(new_tuple):
-					tuple_library[new_tuple] = tuple_to_mutate
-					q.put(new_tuple)
+                #otherwise, check if this tuple is valid, if so add it to the tuple library, and
+                #add to the queue of new tuples for future mutation
+                if validate_tuple(new_tuple):
+                    tuple_library[new_tuple] = tuple_to_mutate
+                    q.put(new_tuple)
 
-	#pprint(tuple_library) #pretty print the final library of all discovered tuples
-	print(f'Completed after {i} iterations')
+    #pprint(tuple_library) #pretty print the final library of all discovered tuples
+    print(f'Completed after {i} iterations')
 
 
 def validate_tuple(t, min_value = 0, max_value = 100):
-	''' Used to prune tuples that are not needed for further mutation in breadth first search.
+    ''' Used to prune tuples that are not needed for further mutation in breadth first search.
 
-	Filters out tuples with non-integer values, invalid values (divide by zero, etc.),
-	or where values exceed lower/upper range limits (we don't need to consider 720 factorial
-	for example). 
-	
-	Arguments:
-	t {tuple} -- Tuple to validate
-	min_value {int} -- Tuples containing values less than this amount will be filtered out from BFS
-	max_value {int} -- Tuples containing values greater than this amount will be filtered out from BFS
-	'''
+    Filters out tuples with non-integer values, invalid values (divide by zero, etc.),
+    or where values exceed lower/upper range limits (we don't need to consider 720 factorial
+    for example). 
+    
+    Arguments:
+    t {tuple} -- Tuple to validate
+    min_value {int} -- Tuples containing values less than this amount will be filtered out from BFS
+    max_value {int} -- Tuples containing values greater than this amount will be filtered out from BFS
+    '''
 
-	for val in t:
-		#throw out tuples that don't meet this condition
-		if val is None or not float(val).is_integer() or val < min_value or abs(val) > max_value:
-			return False
+    for val in t:
+        #throw out tuples that don't meet this condition
+        if val is None or not float(val).is_integer() or val < min_value or abs(val) > max_value:
+            return False
 
-	return True
+    return True
 
 
 def special_divide(x,y):
-	if y == 0:
-		return None #basically tell validate_tuple() it's invalid
+    if y == 0:
+        return None #basically tell validate_tuple() it's invalid
 
-	#this isn't strictly necessary, could return x/y, but this preserves int, 
-	#rather than float conversion. Looks better when you print it out.
-	if x % y == 0:
-		return int(x//y) 
-	else:
-		return None #basically tell validate_tuple() it's invalid
+    #this isn't strictly necessary, could return x/y, but this preserves int, 
+    #rather than float conversion. Looks better when you print it out.
+    if x % y == 0:
+        return int(x//y) 
+    else:
+        return None #basically tell validate_tuple() it's invalid
 
 
 if __name__ == '__main__':
-	import argparse
+    import argparse
 
-	def input_tuple(s):
-		'''Special validator for --tuple argument'''
-		try:
-			ret = tuple([int(x) for x in s.split(',')])
-			return ret
-		except:
-			raise argparse.ArgumentTypeError("--tuple argument must be in form --tuple x1,x2,x3 ...")
+    def input_tuple(s):
+        '''Special validator for --tuple argument'''
+        try:
+            ret = tuple([int(x) for x in s.split(',')])
+            return ret
+        except:
+            raise argparse.ArgumentTypeError("--tuple argument must be in form --tuple x1,x2,x3 ...")
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--tuple', help="Starting tuple, can be any length. Example Usage: --tuple 3,3,3", type=input_tuple, default = '3,3,3')
-	parser.add_argument('--lower', type=int, default=0, help='Lower bound for integers solutions to detect.')
-	parser.add_argument('--upper', type=int, default=10, help='Upper bound for integers solutions to detect.')
-	parser.add_argument('--maxiter', type=int, default=1000, help='Maximum number of tuples to queue in the breadth-first-search. Intended to cap runtime in case of very large search spaces')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tuple', help="Starting tuple, can be any length. Example Usage: --tuple 3,3,3", type=input_tuple, default = '3,3,3')
+    parser.add_argument('--lower', type=int, default=0, help='Lower bound for integers solutions to detect.')
+    parser.add_argument('--upper', type=int, default=10, help='Upper bound for integers solutions to detect.')
+    parser.add_argument('--maxiter', type=int, default=1000, help='Maximum number of tuples to queue in the breadth-first-search. Intended to cap runtime in case of very large search spaces')
+    args = parser.parse_args()
 
-	find_combinations(starting_tuple = args.tuple, output_lower_bound = args.lower, output_upper_bound = args.upper)
+    find_combinations(starting_tuple = args.tuple, output_lower_bound = args.lower, output_upper_bound = args.upper)
 
-	
+    
